ba7a6dbc358d991564a9b25c62fdfb818aee6c25
theerapat.m <theerapat.m@localhost>
2021-06-16 17:06:31 +0700

    added test suit demo

---
+++ Test Suites/TestSuiteDemo2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ce27677e-5004-4656-8752-8510b40db1f3</testSuiteGuid>
+   <testCaseLink>
+      <guid>51ddd5a6-94de-4fdc-b5e0-4edb98edcd04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
